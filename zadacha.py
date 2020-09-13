5a6e17244537140d443d9186a164be9c28681daa
Vladislav Zharkov <troll75677@gmail.com>
2020-09-13 23:13:20 +0300

    + zadacha

---
+++ zadacha.py
@@ -0,0 +1,55 @@
+n = int(input())
+g = []
+
+def create(child, parent, namesp):
+    if not bool(namesp):
+        namesp.append({parent:[{child:[]}]})
+    else:
+        for sp in namesp:
+            if type(sp) == type({}):
+                for key in sp:
+                    if key == parent: sp[key].append({child:[]})
+                    else: create(child, parent, sp[key])            
+
+def addvar(parent, vari, namesp):
+    if not bool(namesp):
+        namesp.append({parent:[vari]})
+    else:
+        for sp in namesp:
+            if type(sp) == type({}):
+                for key in sp:
+                    if key == parent: sp[key].append(vari)
+                    else: addvar(parent, vari, sp[key]) 
+
+def getvar(parent, vari, namesp, papa = 'None'):
+    if not bool(namesp): return papa
+    else:
+        for sp in namesp:
+            if sp == vari: 
+                return papa
+            else:
+                papa = 'None'
+            if type(sp) == type({}):
+                for key in sp:
+                    if key == parent:
+                        for var in sp[key]:
+                            if var == vari:
+                                papa = key
+                        return papa
+                    else:
+                        papr = getvar(parent, vari, sp[key], key)
+                        if papr != 'None':
+                            papa = papr
+                        return papa
+              
+
+for i in range(n):
+    papa = 'None'
+    cmd, namesp, variab = input().split()
+    if cmd == 'create': create(namesp, variab, g)
+    elif cmd == 'add': addvar(namesp, variab, g)
+    elif cmd == 'get': 
+        papa = getvar(namesp, variab, g, papa)
+        print(papa)
+    else: print('Недопустимый ввод')
+    print(g) 
