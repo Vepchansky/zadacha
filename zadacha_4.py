e4b8c8a7675c03215251fd136e31b4d722efaffd
Vepchansky <71032608+Vepchansky@users.noreply.github.com>
2020-09-23 23:33:33 +0300

    Update zadacha_4.py

--- zadacha_4.py
+++ zadacha_4.py
@@ -7,14 +7,12 @@ def search_parent(parent, child):
     global answer
     answer = False
     if parent != ":" and child != ":":
-        if parent == child: answer = True
-        else:
-            for row in g:
-                if row[0] == child:
-                    for count in row:
-                        if count == parent: answer = True 
-                        elif count != ":" and count != child:
-                            search_parent(parent, count)
+        for row in g:
+            if row[0] == child:
+                for count in row:
+                    if count == parent: answer = True 
+                    elif count != ":" and count != child:
+                        search_parent(parent, count)
 
 
 while n > 0:
@@ -26,7 +24,8 @@ n = int(input())
 
 while n > 0:
     child = input()
-    order.append(child)
+    if child not in order:
+        order.append(child)
     n -= 1
 
 for i in range(len(order)):
@@ -35,3 +34,7 @@ for i in range(len(order)):
             search_parent(order[i], order[j])
             if answer: 
                 print(order[j])
+                break
+    if answer:
+        break
+
